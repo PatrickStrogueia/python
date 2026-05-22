514431a791deb60a2dd455726bcdfffd1115e046
Patrick de Alcantara Strogueia <patrick.strogueia@tivit.com>
2026-05-22 17:00:04 -0300

    Aula 10

--- qapitao-python/variaveis.py
+++ qapitao-python/variaveis.py
@@ -1,4 +1,14 @@
 nome = 'Billy Butcher'
 idade = 30
+altura = 1.78
 
-print(f'O {nome} possui {idade} anos.')
+print(nome)
+print(idade)
+print(altura)
+
+print(f'O {nome} possui {idade} anos e {altura} de altura.')
+
+numero1 = 10
+numero2 = 5
+
+print(numero1 + numero2)
