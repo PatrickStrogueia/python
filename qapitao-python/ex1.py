087dc83dea66859d6e47eac1f3d58b864c724642
Patrick de Alcantara Strogueia <patrick.strogueia@tivit.com>
2026-05-28 07:52:17 -0300

    Ex 01

---
+++ qapitao-python/ex1.py
@@ -0,0 +1,12 @@
+print("--- Bem-vindo ao Sistema de Cadastro ---")
+
+# Todo
+
+nome = input("Qual o seu nome? ")
+comida = input("Qual sua comida favorita? ")
+cidade = input("Onde você mora? ")
+
+print("\n--- Sua Ficha de Cadastro ---")
+print(f"Nome: {nome}")
+print(f"Comida Favorita: {comida}")
+print(f"Mora em: {cidade}")
