1aaf153d3f11a98b759485bba2b964c75bb174c6
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-04 22:36:35 -0400

    Aula 20

---
+++ qapitao-python/menu.py
@@ -0,0 +1,8 @@
+while True:
+    comando = input('Digite sair para fechar o programa: ')
+
+    if comando == 'sair':
+        print('Até logo!')
+        break
+    else:
+        print(f'Você digitou: {comando}')
