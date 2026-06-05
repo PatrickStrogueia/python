422110f162825817ae7abbd0b3af41796341d68d
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-04 21:59:11 -0400

    Aula 15

---
+++ qapitao-python/listas.py
@@ -0,0 +1,10 @@
+lista_de_compras = ['Leite', 'Macarrão', 'Sabonete', 'Ketchup']
+
+numeros_da_sorte = [5, 7, 17, 22]
+
+print(lista_de_compras)
+
+print(numeros_da_sorte)
+
+print(lista_de_compras[3])
+
