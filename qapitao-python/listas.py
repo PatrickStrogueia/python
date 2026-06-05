755972300fe10478e2d36a270040cc8e5acc5051
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-04 22:09:52 -0400

    Aula 16

--- qapitao-python/listas.py
+++ qapitao-python/listas.py
@@ -1,4 +1,4 @@
-lista_de_compras = ['Leite', 'Macarrão', 'Sabonete', 'Ketchup']
+lista_de_compras = ['Leite', 'Macarrão', 'Sabonete', 'Ketchup', 'Açúcar']
 
 numeros_da_sorte = [5, 7, 17, 22]
 
@@ -6,5 +6,10 @@ print(lista_de_compras)
 
 print(numeros_da_sorte)
 
+print(lista_de_compras[0])
+print(lista_de_compras[1])
+print(lista_de_compras[2])
 print(lista_de_compras[3])
 
+for item in lista_de_compras:
+    print(item)
