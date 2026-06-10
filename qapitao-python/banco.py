d14cd6177c86855ede09421d9127d4ac6ef51728
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-10 17:24:34 -0400

    Aula 29

--- qapitao-python/banco.py
+++ qapitao-python/banco.py
@@ -7,12 +7,25 @@ camelCase
 # atributos (self.titular) e argumentos (titular)
 
 class ContaBancaria:
+    
+    # construtor
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo = self.saldo + valor
+        print(f'Depósito de R$ {valor} realizado com sucesso!')
+
+    def consulta_saldo(self):
+        print(f'Saldo atual de {self.titular}: R$ {self.saldo}.')
 
 patrick = ContaBancaria('Patrick', 1000)
 maria = ContaBancaria('Maria', 2000)
 
 print(patrick.titular)
 print(patrick.saldo)
+
+patrick.consulta_saldo()
+patrick.depositar(500)
+patrick.consulta_saldo()
