3f300ef0a08b5ee1a6fd448940bad3067154467d
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-10 16:44:09 -0400

    Aula 25

--- qapitao-python/funcoes.py
+++ qapitao-python/funcoes.py
@@ -1,4 +1,15 @@
-def mostrar_nome(nome):
-    print(nome)
+# def mostrar_nome(nome):
+#     print(nome)
 
-mostrar_nome('Patrick')
+# mostrar_nome('Patrick')
+
+# print('Strogueia')
+
+def converter_para_real(valor_em_dolar):
+    cotacao = 5.50
+    resultado = valor_em_dolar * cotacao
+    return resultado
+
+valor_convertido = converter_para_real(100)
+
+print(valor_convertido)
