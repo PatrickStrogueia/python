d70b8425134b166f70ef93cbe46c354d0c3e1a8a
Patrick Strogueia <patrickdealcantara@gmail.com>
2026-06-10 16:52:28 -0400

    Aula 26

--- qapitao-python/funcoes.py
+++ qapitao-python/funcoes.py
@@ -5,11 +5,21 @@
 
 # print('Strogueia')
 
-def converter_para_real(valor_em_dolar):
-    cotacao = 5.50
-    resultado = valor_em_dolar * cotacao
-    return resultado
+# def converter_para_real(valor_em_dolar):
+#     cotacao = 5.50
+#     resultado = valor_em_dolar * cotacao
+#     return resultado
 
-valor_convertido = converter_para_real(100)
+# valor_convertido = converter_para_real(100)
 
-print(valor_convertido)
+# print(valor_convertido)
+
+def criar_email_corporativo(nome: str, empresa: str):
+    email = f'{nome}@{empresa}.com'
+    return email.lower()
+
+email_patrick = criar_email_corporativo('Patrick', 'TIVIT')
+email_strogueia = criar_email_corporativo('Strogueia', 'Google')
+email_maria = criar_email_corporativo('Maria', 'Tesla')
+
+print(email_patrick, email_strogueia, email_maria)
